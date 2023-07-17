f7f9d10ffc3c507b91b2664093df14691ad8b833
HAKD@1 <dubeyad2003@gmail.com>
2023-07-17 19:06:42 +0530

    Create about page

---
+++ src/app/about/page.js
@@ -0,0 +1,40 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our movie review website! We are your go-to destination for
+        all things cinema. Whether you're a casual moviegoer or a passionate
+        film aficionado, we've got you covered with insightful and comprehensive
+        reviews of the latest releases.
+      </p>
+      <p>
+        At MovieReviews, we believe that movies have the power to entertain,
+        inspire, and provoke thought. Our dedicated team of film enthusiasts is
+        committed to providing you with unbiased and engaging reviews that delve
+        deep into the heart of each film. We aim to offer a platform where you
+        can find valuable information and make informed decisions about what to
+        watch.
+      </p>
+      <p>
+        With a vast library of reviews spanning various genres and eras, we
+        strive to cater to diverse tastes and preferences. From Hollywood
+        blockbusters to independent gems, we take pride in exploring the full
+        spectrum of cinematic experiences. Our reviews not only focus on the
+        technical aspects of filmmaking but also delve into the narrative,
+        performances, direction, and overall impact of each movie.
+      </p>
+      <p>
+        We understand that everyone has unique tastes, and that's why we provide
+        a rating system that considers various aspects of a film. Our ratings
+        reflect the overall quality, entertainment value, and artistic merit,
+        helping you make an informed decision about which movies are worth your
+        time and which ones might not be your cup of tea.
+      </p>
+      <p>
+        So grab your popcorn, sit back, and let us guide you through the
+        wonderful world of movies.
+      </p>
+    </div>
+  );
+}
